9313997286cb123ef180a0d6b7652c13120e9754
Brandon Bradley <bradleytastic@gmail.com>
2014-02-01 14:37:43 -0600

    Install The Foreman during bootstrap

--- site/bootstrap.pp
+++ site/bootstrap.pp
@@ -1,3 +1,22 @@
 class { 'apt':
   always_apt_update => true,
 }
+
+apt::key { 'foreman':
+  key        => 'E775FF07',
+  key_source => 'http://deb.theforeman.org/foreman.asc',
+}->
+apt::source { 'foreman':
+  location    => 'http://deb.theforeman.org/',
+  release     => 'wheezy',
+  repos       => '1.4',
+  include_src => false,
+}->
+package { 'foreman-installer':
+  ensure => present,
+}~>
+exec { 'foreman-installer':
+  path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  logoutput   => true,
+  refreshonly => true,
+}

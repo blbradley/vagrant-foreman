ca8fb32a415a4449e21331310fca4230f4c55199
Brandon Bradley <bradleytastic@gmail.com>
2014-02-05 22:28:43 -0600

    Move foreman bootstrap to stage before main

--- site/bootstrap.pp
+++ site/bootstrap.pp
@@ -1,23 +1,33 @@
-class { 'apt':
+stage { 'foreman':
+  before => Stage['main'],
 }
 
-apt::key { 'foreman':
-  key        => 'E775FF07',
-  key_source => 'http://deb.theforeman.org/foreman.asc',
-}->
-apt::source { 'foreman':
-  location    => 'http://deb.theforeman.org/',
-  release     => 'wheezy',
-  repos       => '1.4',
-  include_src => false,
+class { 'foreman':
+  stage => foreman,
 }
 
-package { 'foreman-installer':
-  ensure => present,
-  require => Class['apt::update'],
-}~>
-exec { 'foreman-installer':
-  path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  logoutput   => true,
-  refreshonly => true,
+class foreman {
+  class { 'apt':
+  }
+  
+  apt::key { 'foreman':
+    key        => 'E775FF07',
+    key_source => 'http://deb.theforeman.org/foreman.asc',
+  }->
+  apt::source { 'foreman':
+    location    => 'http://deb.theforeman.org/',
+    release     => 'wheezy',
+    repos       => '1.4',
+    include_src => false,
+  }
+  
+  package { 'foreman-installer':
+    ensure => present,
+    require => Class['apt::update'],
+  }~>
+  exec { 'foreman-installer':
+    path        => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    logoutput   => true,
+    refreshonly => true,
+  }
 }
